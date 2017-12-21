03418e913f9bf35d0e11f61693b2599a917b28a2
Diana <dianastanciu13@yahoo.co.uk>
2017-12-21 14:16:02 +0200

    add nav dropdown menu behaviour

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-    
+    $(".hom p").on("mouseenter", function() {
+        $(".hom > .hidden-drop-menu").removeClass("showHiddenMenuR").addClass("showHiddenMenu");
+    });
+
+    $(".hom").on("mouseleave", function() {
+        $(".hom > .hidden-drop-menu").removeClass("showHiddenMenu").addClass("showHiddenMenuR");
+    });
+
+    $(".proj p").on("mouseenter", function() {
+        $(".proj > .hidden-drop-menu").removeClass("showHiddenMenuR").addClass("showHiddenMenu");
+    });
+
+    $(".proj").on("mouseleave", function() {
+        $(".proj > .hidden-drop-menu").removeClass("showHiddenMenu").addClass("showHiddenMenuR");
+    });
+
+
+    $(".serv p").on("mouseenter", function() {
+        $(".serv > .hidden-drop-menu").removeClass("showHiddenMenuR").addClass("showHiddenMenu");
+    });
+
+    $(".serv").on("mouseleave", function() {
+        $(".serv > .hidden-drop-menu").removeClass("showHiddenMenu").addClass("showHiddenMenuR");
+    });
+
+    $(".blg p").on("mouseenter", function() {
+        $(".blg > .hidden-drop-menu").removeClass("showHiddenMenuR").addClass("showHiddenMenu");
+    });
+
+    $(".blg").on("mouseleave", function() {
+        $(".blg > .hidden-drop-menu").removeClass("showHiddenMenu").addClass("showHiddenMenuR");
+    });
+
 });
